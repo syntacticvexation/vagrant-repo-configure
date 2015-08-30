81d503f1d5d894ef7c45037329ebfe38dfe77e0c
Syntactic Vexation <syntacticvexation@gmail.com>
2015-08-30 13:09:21 +1000

    fixed logic, multiple repo lines can now be added properly

--- lib/vagrant-repo-configure/middleware.rb
+++ lib/vagrant-repo-configure/middleware.rb
@@ -8,8 +8,9 @@ module Vagrant
 		  def call(env)
 		  	vm      = env[:vm] || env[:machine]
 
+			vm.communicate.sudo('mv /etc/apt/sources.list /etc/apt/sources.list.old')
+
 		  	vm.config.repos.apt_lines.each do |line|
-		  		vm.communicate.sudo('mv /etc/apt/sources.list /etc/apt/sources.list.old')
 		  		vm.communicate.sudo("echo #{line} >> /etc/apt/sources.list")
 		  		env[:ui].info "Added #{line} to sources list"
 		  	end
